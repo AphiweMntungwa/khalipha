b8ae9bb63863c6a67ae4f52e32c46587ba3b902b
AphiweMntungwa <aphiwemntungwa66@gmail.com>
2022-04-19 16:11:29 +0200

    bugs

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'build')))
 
-const port = process.env.port || 3001
+const port = process.env.PORT || 3001
 app.listen(port, () => {
     console.log('LISTENING, PORT', port)
 })
